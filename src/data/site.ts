f128f144fa55b6d18fea5bffc8a67574ed5cbdae
brendon.wang <brendon.wang@gmail.com>
2026-09-13 16:29:37 -0700

    Refine navigation and control styling

--- src/data/site.ts
+++ src/data/site.ts
@@ -9,7 +9,7 @@ export const navigation = [
   { label: 'SACC', href: '/sacc' },
   { label: 'About us', href: '/about' },
   { label: 'Sponsors', href: '/sponsors' },
-  { label: 'Organize', href: '/join' },
+  { label: 'Join us', href: '/join' },
 ];
 
 export const sponsors = [
